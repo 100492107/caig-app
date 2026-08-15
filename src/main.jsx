8dab110b76dca1faba5ee079b3769a9f6c830632
Magdalena Michalczyk <100492107@unimail.derby.ac.uk>
2026-08-15 16:01:14 +0100

    Route Creative Engine workspace without touching main app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import CreativeEngineWorkspace from './CreativeEngineWorkspace.jsx'
+
+const isCreativeRoute = window.location.pathname === '/creative' || window.location.pathname === '/creative/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isCreativeRoute ? <CreativeEngineWorkspace /> : <App />}
   </StrictMode>,
 )
