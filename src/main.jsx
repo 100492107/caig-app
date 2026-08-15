560301530b381d373263eb5c9c99da7e7832ea09
Magdalena Michalczyk <100492107@unimail.derby.ac.uk>
2026-08-15 16:17:57 +0100

    Add launcher and route to Creative Engine V2

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import CreativeEngineWorkspace from './CreativeEngineWorkspace.jsx'
+import AppLanding from './AppLanding.jsx'
+import CreativeEngineWorkspace from './CreativeEngineWorkspaceV2.jsx'
 
-const isCreativeRoute = window.location.pathname === '/creative' || window.location.pathname === '/creative/'
+const path = window.location.pathname.replace(/\/+$/, '') || '/'
+const component = path === '/creative'
+  ? <CreativeEngineWorkspace />
+  : path === '/main-app'
+    ? <App />
+    : <AppLanding />
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {isCreativeRoute ? <CreativeEngineWorkspace /> : <App />}
-  </StrictMode>,
+  <StrictMode>{component}</StrictMode>,
 )
