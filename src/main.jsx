6de976195773f8561be337fa19ddde3781ff8f69
Magdalena Michalczyk <100492107@unimail.derby.ac.uk>
2026-08-15 16:21:37 +0100

    Use main app shell with home button

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import AppLanding from './AppLanding.jsx'
+import MainAppShell from './MainAppShell.jsx'
 import CreativeEngineWorkspace from './CreativeEngineWorkspaceV2.jsx'
 
 const path = window.location.pathname.replace(/\/+$/, '') || '/'
 const component = path === '/creative'
   ? <CreativeEngineWorkspace />
   : path === '/main-app'
-    ? <App />
+    ? <MainAppShell />
     : <AppLanding />
 
 createRoot(document.getElementById('root')).render(
