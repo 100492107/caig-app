f1c9f9eac8b2b358494c15a3465894110914349b
Magdalena Michalczyk <100492107@unimail.derby.ac.uk>
2026-08-18 07:06:09 +0100

    Route Track B production to V6 two-stage scene workflow

--- src/CreativeEngineHub.jsx
+++ src/CreativeEngineHub.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import CreativeEngineWorkspace from "./CreativeEngineWorkspaceV5.jsx";
 import TikTokSlideshowStudio from "./TikTokSlideshowStudio.jsx";
 import DistributionLab from "./DistributionLab.jsx";
-import CornerstoneAIAssetsProductionStudio from "./CornerstoneAIAssetsProductionStudioV5.jsx";
+import CornerstoneAIAssetsProductionStudio from "./CornerstoneAIAssetsProductionStudioV6.jsx";
 import TrackBAssetLibrary from "./TrackBAssetLibraryV2.jsx";
 
 const pane = (visible) => ({ display: visible ? "block" : "none" });
