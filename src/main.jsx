ab03f9ba559b59838736f73d54e5123d5eed914e
Magdalena Michalczyk <100492107@unimail.derby.ac.uk>
2026-08-22 12:19:41 +0100

    Apply premium studio presentation to creative engine

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './CreativeStudioTheme.css'
 import AppLanding from './AppLanding.jsx'
 import MainAppShell from './MainAppShell.jsx'
 import CreativeEngineHub from './CreativeEngineHub.jsx'
 
 const path = window.location.pathname.replace(/\/+$/, '') || '/'
 const component = path === '/creative'
-  ? <CreativeEngineHub />
+  ? <div className="creative-studio-surface"><CreativeEngineHub /></div>
   : path === '/main-app'
     ? <MainAppShell />
     : <AppLanding />
