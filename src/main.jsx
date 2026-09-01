721363fae91bfe0b8b25f46624dbdf575f86d4da
Magdalena Michalczyk <100492107@unimail.derby.ac.uk>
2026-09-01 09:27:54 +0100

    Scope Cornerstone instrument styles by active route

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './CreativeStudioTheme.css'
+import './CornerstoneInstrument.css'
 import EnterpriseHome from './EnterpriseHome.jsx'
 import CEOHome from './CEOHome.jsx'
 import AuthGate from './AuthGate.jsx'
@@ -14,6 +15,11 @@ import TrackATerritoryBlock from './TrackATerritoryBlock.jsx'
 
 const path = window.location.pathname.replace(/\/+$/, '') || '/'
 const hour = new Date().getHours()
+
+document.documentElement.dataset.route = path
+
+document.body.dataset.route = path
+
 const component = path === '/creative'
   ? <div className="creative-studio-surface"><CreativeEngineHub /><PersistentGenerations /></div>
   : path === '/outreach'
