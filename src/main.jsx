5808f1675c80fbd951b544bfb0543029d4d631a2
Magdalena Michalczyk <100492107@unimail.derby.ac.uk>
2026-09-01 09:38:48 +0100

    Use new unified Track B application shell

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import EnterpriseHome from './EnterpriseHome.jsx'
 import CEOHome from './CEOHome.jsx'
 import AuthGate from './AuthGate.jsx'
 import MainAppShell from './MainAppShell.jsx'
-import CreativeEngineHub from './CreativeEngineHub.jsx'
+import TrackBApplication from './TrackBApplication.jsx'
 import PersistentGenerations from './PersistentGenerations.jsx'
 import TrackAOutreachWorkspace from './TrackAOutreachWorkspace.jsx'
 import OperatorWorkbench from './OperatorWorkbench.jsx'
@@ -17,11 +17,10 @@ const path = window.location.pathname.replace(/\/+$/, '') || '/'
 const hour = new Date().getHours()
 
 document.documentElement.dataset.route = path
-
 document.body.dataset.route = path
 
 const component = path === '/creative'
-  ? <div className="creative-studio-surface"><CreativeEngineHub /><PersistentGenerations /></div>
+  ? <div className="creative-studio-surface"><TrackBApplication /><PersistentGenerations /></div>
   : path === '/outreach'
     ? <TrackAOutreachWorkspace />
     : path === '/main-app'
